27963ca7711b048088aafc6ab2a38571cd260fae
Larry Ketone <kofie15@live.com>
2018-02-27 18:41:47 +0000

    Owner Verification layout and code

---
+++ src/main/java/com/example/larry/lotowner/OwnerVerification.java
@@ -0,0 +1,23 @@
+package com.example.larry.lotowner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class OwnerVerification extends AppCompatActivity {
+
+    EditText firstEditText;
+    EditText secondEditText;
+
+    Button
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_owner_verification);
+
+    }
+}
